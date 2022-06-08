bbb1cf2a30b868418a962927c4db812f5755cc03
fluorke <fluoropla@gmail.com>
2022-06-09 00:29:03 +0300

    add recursive linear search

--- korman_entry/linear_search.js
+++ korman_entry/linear_search.js
@@ -1,6 +1,6 @@
 let a = [1,2,3,4,5,6,7,8];
 
-const Sentine_lLinear_Search = (A,n,x) => {
+const Sentinel_Linear_Search = (A,n,x) => {
   let last = A[n - 1];
   A[n - 1] = x;
   let i = 0;
---
+++ korman_entry/recursive_linear_search.js
@@ -0,0 +1,12 @@
+let a = [1,2,3,4,5,6,7,8];
+
+const recursive_linear_search = (A, n, i, x) => {
+  if (i > n) {
+    return "not-found";
+  } else if (i <= n) {
+    if (A[i] == x) return i;
+    else return recursive_linear_search(A, n, i+1, x);
+  }
+}
+
+console.log(recursive_linear_search(a, 8, 0, 6));
