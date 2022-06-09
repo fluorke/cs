dfa5900fbe7185864bd5bec38ef95e9ebb3f0775
fluorke <fluoropla@gmail.com>
2022-06-09 12:59:14 +0300

    bsearch

---
+++ korman_entry/binary_search.js
@@ -0,0 +1,17 @@
+let a = [1,2,3,4,5,6,7,8];
+
+const binary_search = (A, n, x) => {
+  let q, p = 1, r = n;
+
+  while (p <= r) {
+    q = Math.trunc((p + r) / 2);
+    if (A[q] == x) return q;
+    else if (A[q] > x) {
+      r = q - 1;
+    } else if (A[q] < x) {
+      p = q + 1;
+    } else return "not-found";
+  }
+}
+
+console.log(binary_search(a, 8, 5));
